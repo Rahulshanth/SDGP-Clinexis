334ee7131fdbed0d025c7ec04c278b00b2462e2f
Nadithi Moonasingha <nadithimoonasingha@Nadithis-MacBook-Air.local>
2026-02-17 21:53:52 +0530

    Configured MongoDB Atlas with NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
