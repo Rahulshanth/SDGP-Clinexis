d8801990b2963054a6aa9d471f0397704f12abfb
Nadithi Moonasingha <nadithimoonasingha@Nadithis-MacBook-Air.local>
2026-02-17 22:40:12 +0530

    refactor: move MongoDB connection logic to DatabaseModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
-    MongooseModule.forRoot(process.env.MONGO_URI as string),
-
+    DatabaseModule,
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
+
+
+
