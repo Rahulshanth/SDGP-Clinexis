96e10a50387e454711985d61554d530761a176c7
Nadithi <moonasinghanadithi@gmail.com>
2026-02-26 20:45:20 +0530

    Remove node_modules from tracking

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+//import {AppController} from './app.controller'; <--Don't delete these comments
+//import  {AppService} from './app.service'; <--Don't delete these comments
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DoctorModule } from './doctors/doctor.module';
 import { PatientModule } from './patient/patient.module';
 import { PharmacyModule } from './pharmacy/pharmacy.module';
 import { SummarizationModule } from './summarization/summarization.module';
-import { DatabaseModule } from './database/database.module';
 import { ConsultationsModule } from './consultations/consultations.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true,}), // Added by Nadithi
+    ConfigModule.forRoot({isGlobal: true,}),// Added by Nadithi
+    
     MongooseModule.forRoot(process.env.MONGODB_URI!),
-    AuthModule,
+    
+    
     UsersModule,
-    DoctorModule,
-    PatientModule,
-    PharmacyModule,
-    SummarizationModule,
-    DatabaseModule,
-    ConsultationsModule,
+    AuthModule,
+    DoctorModule,PatientModule, //by rahul
+    PharmacyModule,  //by vinidi
+    SummarizationModule, //keep this - rivithi
   ],
+  //controllers: [AppController], <--Don't delete these comments
+  //providers: [AppService], <--Don't delete these comments
 })
 export class AppModule {}
