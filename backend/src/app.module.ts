c315e01f4588084654d6ac0a803ec4b2c97072ff
Vinidi Rathnayake <vinidirathnayake@gmail.com>
2026-02-26 08:41:27 +0530

    fix: restored app.module.ts to correct team structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+/*import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
@@ -19,6 +19,37 @@ import { PharmacyModule } from './pharmacy/pharmacy.module';
     AuthModule,
     SummarizationModule,   //keep this
     PharmacyModule,        // add this
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
+})
+export class AppModule {}*/
+
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+// import { AppController } from './app.controller';  <-- Don't delete these comments
+// import { AppService } from './app.service';        <-- Don't delete these comments
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+//import { DoctorModule } from './doctor/doctor.module';
+import { SummarizationModule } from './summarization/summarization.module';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    UsersModule,
+    AuthModule,
+    SummarizationModule,  // keep this - Rivithi
+    PharmacyModule,       // add this - Vinidi
+   // DoctorModule,
   ],
   // controllers: [AppController],
   // providers: [AppService],
