dffab8a0e2a4ed3de22b8a03ae2db621afba14c0
Vinuki Buwaneka <vinukibuwaneka@gmail.com>
2026-02-26 13:55:23 +0530

    Update NLP feature and MongoDB integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { NlpModule } from './nlp/nlp.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(), // load .env file
+    MongooseModule.forRoot(process.env.MONGO_URI!), // connect MongoDB
+    AuthModule,
+    NlpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
