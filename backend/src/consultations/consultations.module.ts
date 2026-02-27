4b839d953f0070392ee59b1eaeaf3577a022b494
Rahulshanth <yogarajahrahulshanth@gmail.com>
2026-02-27 20:19:06 +0530

    Finish the MongoDB schema implementations

--- backend/src/consultations/consultations.module.ts
+++ backend/src/consultations/consultations.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { ConsultationsController } from './consultations.controller';
 import { ConsultationsService } from './consultations.service';
+import { MongooseModule } from '@nestjs/mongoose';  // Updated according schemas by rahul
+import {
+  Consultation,
+  ConsultationSchema,
+} from './schemas/consultation.schema'; // Updated according to schemas by rahul
+
 
 @Module({
   imports: [
@@ -10,6 +16,9 @@ import { ConsultationsService } from './consultations.service';
         fileSize: 20 * 1024 * 1024, // 20MB
       },
     }),
+    MongooseModule.forFeature([
+      { name: Consultation.name, schema: ConsultationSchema },
+    ]), // Updated according to schemas by rahul
   ],
   controllers: [ConsultationsController],
   providers: [ConsultationsService],
