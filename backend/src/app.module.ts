3cb438c41053d794c595f2ce7a69aa7f35443e83
Nadithi <moonasinghanadithi@gmail.com>
2026-02-27 13:20:46 +0530

    Fix app.module before switching branch

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
+//import { AppController } from './app.controller';  <-- Don't delete these comments
+//import { AppService } from './app.service';   <-- Don't delete these comments
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DoctorModule } from './doctors/doctor.module';
-import { PatientModule } from './patient/patient.module';
+import {PatientModule} from './patient/patient.module'
+import { SummarizationModule } from './summarization/summarization.module';
 import { PharmacyModule } from './pharmacy/pharmacy.module';
-
-import { ConfigService } from '@nestjs/config'; //Added by Nadithi
 
 @Module({
   imports: [
@@ -16,35 +16,15 @@ import { ConfigService } from '@nestjs/config'; //Added by Nadithi
       isGlobal: true,
     }),
 
-    MongooseModule.forRootAsync({
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
-    uri: configService.get<string>('MONGO_URI'),
-  }),      //Added by Nadithi on 25th Feb
-}),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
 
     UsersModule,   
-
-import { SummarizationModule } from './summarization/summarization.module';
-import { DatabaseModule } from './database/database.module';
-import { ConsultationsModule } from './consultations/consultations.module';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({isGlobal: true,}), // Added by Nadithi
-    MongooseModule.forRoot(process.env.MONGODB_URI!),
-
     AuthModule,
-    UsersModule,
-    DoctorModule,
-    PatientModule,
-    PharmacyModule,
-    SummarizationModule,
-    DatabaseModule,
-    ConsultationsModule,
+    SummarizationModule,   // 🔹 keep this - Rivithi
+    PharmacyModule,        // 🔹 add this - Vinidi
+    DoctorModule, PatientModule         // By Rahul
   ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
-
-console.log('MONGO_URI:', process.env.MONGO_URI); //Added by Nadithi
-
