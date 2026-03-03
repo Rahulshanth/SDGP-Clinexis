b0f93206497d18019d95f68b33e08d62c7ead3bb
Vidu <vidhuobhasha5000@gmail.com>
2026-03-03 21:22:17 +0530

    Removed frontend implementation from develop PR

--- frontend/services/reminder.service.ts
+++
@@ -1,8 +0,0 @@
-// ALL ADD by VIDU*
-export const createReminder = () => {
-  // frontend placeholder
-};
-
-export const getPendingReminders = () => {
-  // frontend placeholder
-};
--- frontend/types/reminder.ts
+++
@@ -1,7 +0,0 @@
-// ALL ADD by VIDU*
-export interface Reminder {
-  patientId: string;
-  type: "MEDICINE" | "APPOINTMENT";
-  reminderTime: string;
-  message: string;
-}
