7736702ee9c2da4b7d370f3c12a76ba886c18b5a
Vinuki Buwaneka <vinukibuwaneka@gmail.com>
2026-03-08 14:27:28 +0530

    Register core modules in AppModule (auth, users, doctors, patients, pharmacy, reminders, summarization)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,31 @@
 import { Module } from '@nestjs/common';
-import { NlpModule } from './nlp/nlp.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-
+import { RemindersModule } from './reminders/reminders.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DoctorModule } from './doctors/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { SummarizationModule } from './summarization/summarization.module';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // load .env file
-    //MongooseModule.forRoot(process.env.MONGO_URI as string), // connect MongoDB
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    UsersModule,
     AuthModule,
-    NlpModule,
+    RemindersModule, // 🔹 add this - Vidu
+    SummarizationModule, // 🔹 keep this - Rivithi
+    PharmacyModule, // 🔹 add this - Vinidi
+    DoctorModule,
+    PatientModule, // By Rahul
   ],
   controllers: [AppController],
   providers: [AppService],
