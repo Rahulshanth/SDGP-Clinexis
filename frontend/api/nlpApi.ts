e28b6e27fea0d62a4d068d7631610c4ca8ac1b40
Vinuki Buwaneka <vinukibuwaneka@gmail.com>
2026-03-11 11:07:03 +0530

    Vinuki 3/11

---
+++ frontend/api/nlpApi.ts
@@ -0,0 +1,11 @@
+/*import axios from "axios";
+
+const API_BASE_URL = "http://localhost:5001";
+
+export const extractMedicines = async (text: string) => {
+  const response = await axios.post(`${API_BASE_URL}/nlp/extract`, {
+    text: text,
+  });
+
+  return response.data;
+};*/
